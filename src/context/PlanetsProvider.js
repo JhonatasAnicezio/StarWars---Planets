714b64f0e0c16b5152158478f8eae366b99b3f7e
Jhonatas <jhonatasanic@hotmail.com>
2022-09-20 16:39:45 -0300

    feat(PlanetsProvider): fetch api for Table

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 
 function PlanetsProvider({ children }) {
 
-  const context = {};
+  const [ data, setData ] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        const { results } = response;
+        results.map((obj) => {
+          delete obj.residents;
+          return obj;
+        });
+        setData(results);
+      });
+  }, []);
+
+  const context = {
+    data,
+  };
 
   return (
     <PlanetsContext.Provider value={ context }>
