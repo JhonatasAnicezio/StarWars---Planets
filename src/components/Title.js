cb21c649937e1d63a4578bfbe2147208ccd5996c
Jhonatas <jhonatasanic@hotmail.com>
2022-09-21 12:19:14 -0300

    feat(components/Title): create component Title

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Title({ title }) {
+  return (
+    <div>
+      <h1>{ title }</h1>
+    </div>
+  )
+}
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Title;
