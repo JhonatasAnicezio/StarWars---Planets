d7cc054ad5f6944d7d4799786cb14ecd77f71721
Jhonatas <jhonatasanic@hotmail.com>
2022-09-21 12:23:58 -0300

    feat(App): render title in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Table from './components/Table';
+import Title from './components/Title';
 import './App.css';
 
 function App() {
   return (
-    <Table />
+    <div>
+      <Title title={ 'StarWars-Planets' } />
+      <Table />
+    </div>
   );
 }
 
