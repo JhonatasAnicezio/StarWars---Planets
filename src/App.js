9d90b0cb5a8b742c9eaeb5ea05e3262ae43cff11
Jhonatas <jhonatasanic@hotmail.com>
2022-09-22 18:08:58 -0300

    feat(App/Input): ultilize component Input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Table from './components/Table';
 import Title from './components/Title';
 import './App.css';
 import Input from './components/Input';
+import PlanetsContext from './context/PlanetsContext';
 
 function App() {
+  const { setFilterByName } = useContext(PlanetsContext);
   return (
     <div>
       <Title title={ 'StarWars-Planets' } />
-      <Input />
+      <Input
+        text='Procure por um planeta'
+        type='text'
+        name='search-name'
+        func={ setFilterByName }
+      />
       <Table />
     </div>
   );
