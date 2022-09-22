98c86a88611554ea3bb6d2a55ecf5b90f74a40d4
Jhonatas <jhonatasanic@hotmail.com>
2022-09-22 10:16:21 -0300

    feat(App): render component Input in component main App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Table from './components/Table';
 import Title from './components/Title';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div>
       <Title title={ 'StarWars-Planets' } />
+      <Input />
       <Table />
     </div>
   );
