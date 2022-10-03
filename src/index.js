573f6c8996ea5ede8078b10ea82b6eb4c384cfe7
Jhonatas <jhonatasanic@hotmail.com>
2022-10-03 13:39:48 -0300

    feat(index/context): add context Provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PlanetsProvider from './context/PlanetsProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PlanetsProvider>
+      <App />
+    </PlanetsProvider>
   </React.StrictMode>
 );
 
