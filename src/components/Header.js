43dca87ed0d197c36196bb31188222e0f57fdcf6
Jhonatas <jhonatasanic@hotmail.com>
2022-10-03 18:15:53 -0300

    feat(components/Header): create component Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Projeto Star Wars - Trybe</h1>
+    </header>
+  );
+}
+
+export default Header;
