c7cc89fa1ec5fbeaac25265534bce95c2c9c328f
Jhonatas <jhonatasanic@hotmail.com>
2022-10-03 16:04:11 -0300

    feat(context/PlanetsProvider): add fetch api for Planets

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PlanetsContext from './PlanetsContext';
 import PropTypes from 'prop-types';
 
-function PlanetsProvider(children) {
+function PlanetsProvider({ children }) {
+  const [ planets, setPlanets ] = useState([]);
 
-  const context = {};
+  useEffect(() => {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    fetch(url).then((response) => response.json())
+      .then((response) => {
+        const { results } = response;
+        results.map((obj) => {
+          delete obj.residents;
+          return obj;
+        });
+        setPlanets(results);
+      });
+  }, []);
+
+  const context = {
+    planets,
+  };
 
   return (
     <PlanetsContext.Provider value={ context }>
-      { children }
+      {children}
     </PlanetsContext.Provider>
   );
 }
