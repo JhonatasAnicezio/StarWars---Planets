1896f6147a9e1338523e65b813be5e76b0eed475
Jhonatas <jhonatasanic@hotmail.com>
2022-10-05 12:28:25 -0300

    feat(context/PlanetsProvider): add new state and func filter results

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -4,22 +4,32 @@ import PropTypes from 'prop-types';
 
 function PlanetsProvider({ children }) {
   const [ planets, setPlanets ] = useState([]);
+  const [ filter, setFilter ] = useState({
+    filterByName: {
+      name: '',
+    }
+  });
 
   useEffect(() => {
     const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
     fetch(url).then((response) => response.json())
       .then((response) => {
         const { results } = response;
+        const { name } = filter.filterByName;
         results.map((obj) => {
           delete obj.residents;
           return obj;
         });
-        setPlanets(results);
+        setPlanets(results.filter((planet) => {
+          const str = planet.name.toLowerCase();
+          return str.includes(name);
+        } ));
       });
-  }, []);
+  }, [filter]);
 
   const context = {
     planets,
+    setFilter,
   };
 
   return (
