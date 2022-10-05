c7db421d7e038cb81c8103849a7640c99e1e4d9b
Jhonatas <jhonatasanic@hotmail.com>
2022-10-05 12:54:23 -0300

    feat(Header/Input): render Input in component Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Input from './Input.js';
 import './Header.css';
 
 function Header() {
   return (
     <header>
       <h1>Projeto Star Wars - Trybe</h1>
+      <Input />
     </header>
   );
 }
