6f9385f16a700cb7101cfbbe662874e8dbe95d16
Jhonatas <jhonatasanic@hotmail.com>
2022-10-07 10:34:32 -0300

    refactor(components/Header): alter render component Input props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Input from './Input.js';
 import './Header.css';
-import ListFilters from './ListFilters.js';
+import PlanetsContext from '../context/PlanetsContext.js';
 
 function Header() {
+  const { setFilter, filter } = useContext(PlanetsContext);
+
+  const inputName = ( value ) => {
+    setFilter({ 
+      ...filter,
+      filterByName: {
+        name: value,
+      }
+    });
+  };
+
   return (
     <header>
-      <h1>Projeto Star Wars - Trybe</h1>
-      <Input />
-      <ListFilters />
+      <div className='container-title'>
+        <h1>Projeto Star Wars - Trybe</h1>
+        <Input
+          className='input-Name'
+          type='text'
+          name='filterName'
+          func={ inputName }
+        />
+      </div>
     </header>
   );
 }
