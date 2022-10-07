fb8052bf1a385a2da80cd4f3e52f211e606ae0ad
Jhonatas <jhonatasanic@hotmail.com>
2022-10-07 10:23:43 -0300

    refactor(components/Input): alter component Input in func component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,30 @@
-import React, { useContext } from 'react';
-import PlanetsContext from '../context/PlanetsContext';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Input.css';
 
-function Input() {
-  const { setFilter } = useContext(PlanetsContext);
+function Input({ className, type, name, func }) {
+
+  function handleOnChange({ target }) {
+    const { value } = target;
+    func(value.toLowerCase());
+  }
 
   return (
     <input
-      className='input-Name'
-      type='text'
-      name='filterName'
-      id='filterName'
-      onChange={ ({ target }) =>
-        setFilter({ filterByName: {
-          name: target.value.toLowerCase(),
-        } })
-      }
+      className={ className }
+      type={ type }
+      name={ name }
+      id={ name }
+      onChange={ handleOnChange }
     />
   );
 }
+
+Input.propTypes = {
+  className: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
+};
 
 export default Input;
