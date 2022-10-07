22bb8c2e1bc23df7bc6d14c04e8d938ae027da64
Jhonatas <jhonatasanic@hotmail.com>
2022-10-07 10:30:24 -0300

    feat(components/ListFilters): create component ListFilters

--- src/components/ListFilters.js
+++ src/components/ListFilters.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+import Button from './Button';
+import Input from './Input';
+import Select from './Select';
+import './ListFilters.css';
 
 function ListFilters() {
-  return (
-    <div>
+  const { setColumn, setComparison, setValue,
+  } = useContext(PlanetsContext);
+  const columnArr = ['population', 'orbital_period',
+    'diameter','rotation_period', 'surface_water'];
+  const operatorArr = [ 'maior que', 'menor que', 'igual a'];
 
+  return (
+    <div className='container-filter'>
+      <Select
+        text='Coluna'
+        name='column'
+        options={ columnArr }
+        func={ setColumn }
+      />
+      <Select
+        text='Operador'
+        name='operator'
+        options={ operatorArr }
+        func={ setComparison }
+      />
+      <Input
+        className='input-Number'
+        type='text'
+        name='filterNumber'
+        func={ setValue }
+      />
+      <Button />
     </div>
   );
 }
