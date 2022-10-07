98643d9b7248d848526e1592f3c3dadd28d0d02d
Jhonatas <jhonatasanic@hotmail.com>
2022-10-07 06:48:16 -0300

    feat(components/Header): import component ListFilters in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Input from './Input.js';
 import './Header.css';
+import ListFilters from './ListFilters.js';
 
 function Header() {
   return (
     <header>
       <h1>Projeto Star Wars - Trybe</h1>
       <Input />
+      <ListFilters />
     </header>
   );
 }
