a6b93b021db28be4b2c73d42f71ff43eb8a8d01d
Jhonatas <jhonatasanic@hotmail.com>
2022-10-13 13:14:17 -0300

    feat(components/ButtonOrder): create new component Button

---
+++ src/components/ButtonOrder.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ButtonOrder.css';
+
+function ButtonOrder() {
+  return (
+    <div>
+      <button
+        className='button-order'
+        type='button'
+      >
+        ORDENAR
+      </button>
+    </div>
+  );
+}
+
+export default ButtonOrder;
