4d3c93363c9288abacc9e7769a2d5aad50cde898
Jhonatas <jhonatasanic@hotmail.com>
2022-10-13 11:38:03 -0300

    feat(components/Button): create func disabled and remove filter repet

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 import './Button.css';
 
 function Button() {
-  const { column, comparison, value, setFilter, filter } = useContext(PlanetsContext);
+  const { column, comparison, value, setFilter,
+    filter, columnArr, setColumn } = useContext(PlanetsContext);
+  const [ disabled, setDisabled ] = useState(false);
 
   function addFilters() {
     setFilter({
@@ -17,13 +19,26 @@ function Button() {
         }
       ]
     });
+
+    const indice = columnArr.indexOf(column);
+    columnArr.splice(indice, 1);
+    setColumn(columnArr[0]);
   }
+
+  useEffect(() => {
+    if (columnArr.length == 0) {
+      setDisabled(true);
+    } if (columnArr.length > 0) {
+      setDisabled(false);
+    }
+  });
 
   return (
     <button
       className='main-button'
       type='button'
       onClick={ addFilters }
+      disabled={ disabled }
     >
       FILTRAR
     </button>
